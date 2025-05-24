304f3c207756710b85b29acc6e7e6a9b1e822ad2
Shreyansh <shreyanshchaudhary99@gmail.com>
2025-05-24 14:38:47 +0530

    adopter sign up update-2

--- src/pages/SignUpAdopter.jsx
+++ src/pages/SignUpAdopter.jsx
@@ -28,41 +28,51 @@ function SignUpAdopter()
     };
     
     return(
-        <div className="flex items-center justify-center min-h-screen bg-cover" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1641895964758-3e4374714a8c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')` }}>
-            <div className="flex flex-col w-full max-w-lg p-6 m-10 bg-gradient-to-r from-[#ebf4f5] to-[#b5c6e0] border-2 border-black rounded-xl shadow-4xl ">
-                <h2 className="text-2xl underline font-semibold font-serif text-black text-center mb-4 shadow-md p-2 border border-black rounded-md">Adopter Signup Form</h2>
-                <div className="border border-black rounded-md shadow-md my-2">
+        <div className="flex items-center justify-center min-h-screen bg-[#450920]">
+            <div className="flex flex-col w-full max-w-lg p-6 m-10 bg-[#fbfefb] border-2 border-black rounded-xl shadow-4xl ">
+                <h2 className="text-2xl underline font-semibold font-serif text-black text-center mb-4 bg-[#f8f7ff] shadow-md p-2 border border-black rounded-md">Adopter Signup Form</h2>
+                <div className="border border-black rounded-md bg-blue-50 shadow-md my-2">
                     <h3 className="text-lg underline font-semibold font-serif ml-4 mt-2 shadow-sm">Personal Information</h3>
                     <input name="name" value={adopterData.name} onChange={handleChange} className="border border-black rounded-md p-1 m-4" placeholder="Full Name of the adopter"></input>
                     <input name="religion" value={adopterData.religion} onChange={handleChange} className="border border-black rounded-md p-1 m-4" placeholder="Religion"></input>
 
-                    <select name="gender" value={adopterData.gender} onChange={handleChange} className="border border-black rounded-md p-1 m-4">
-                        <option value="">Gender</option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                        <option value="others">Others</option>
-                    </select>
+                    <div className="flex flex-wrap gap-2">
+                        <div>
+                            <label className="block ml-6 text-sm font-medium text-gray-700">Gender</label>
+                            <select name="gender" value={adopterData.gender} onChange={handleChange} className="border border-black rounded-md p-1 ml-4 mr-4 mb-4 mt-1">
+                                <option value="">Gender</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                                <option value="others">Others</option>
+                            </select>
+                        </div>
 
-                    <input name="dob" value={adopterData.dob} onChange={handleChange} className="border border-black rounded-md p-1 m-2" placeholder="Enter Date Of Birth" type="date"></input>
+                        <div>
+                            <label className="block ml-6 text-sm font-medium text-gray-700">Date Of Birth</label>
+                            <input name="dob" value={adopterData.dob} onChange={handleChange} className="border border-black rounded-md p-1 mt-1" placeholder="Enter Date Of Birth" type="date"></input>
+                        </div>
 
-                    <select name="maritalStatus" value={adopterData.maritalStatus} onChange={handleChange} className="border border-black rounded-md p-1 m-4">
-                        <option value="">Marital Status</option>
-                        <option value="married">Married</option>
-                        <option value="single">Single</option>
-                        <option value="divorced">Divorced</option>
-                    </select>
-
+                        <div>
+                            <label className="block ml-6 text-sm font-medium text-gray-700">Marital Status</label>
+                            <select name="maritalStatus" value={adopterData.maritalStatus} onChange={handleChange} className="border border-black rounded-md p-1 mt-1 ml-4">
+                                <option value="">Marital Status</option>
+                                <option value="married">Married</option>
+                                <option value="single">Single</option>
+                                <option value="divorced">Divorced</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
                
                 
-                <div className="border border-black rounded-md shadow-md my-2">
+                <div className="border border-black rounded-md bg-purple-50 shadow-md my-2">
                     <h3 className="text-lg underline font-semibold font-serif ml-4 mt-2 shadow-sm">Contact Details</h3>
                     <input name="email" value={adopterData.email} onChange={handleChange} className="border border-black rounded-md p-1 m-4" placeholder="Enter your email address"></input>
                     <input name="contact" value={adopterData.contact} onChange={handleChange} className="border border-black rounded-md p-1 m-4" placeholder="Contact Number"></input>
                     <textarea name="address" value={adopterData.address} onChange={handleChange} className="border border-black rounded-md p-1 m-4" placeholder="Enter your Address"></textarea>
                 </div>
 
-                <div className="border border-black rounded-md my-2 shadow-md">
+                <div className="border border-black rounded-md bg-blue-50 my-2 shadow-md">
                     <h3 className="text-lg underline font-semibold font-serif ml-4 mt-2 shadow-sm">Family & Financial Information</h3>
                     <input name="bioChildren" type="number" value={adopterData.bioChildren} onChange={handleChange} className="border border-black rounded-md p-1 m-4" placeholder="Biological children?"></input>
                     <input name="occupation" value={adopterData.occupation} onChange={handleChange} className="border border-black rounded-md p-1 m-4" placeholder="Enter your occupation"></input>
@@ -70,12 +80,12 @@ function SignUpAdopter()
                     <input name="aadhar" value={adopterData.aadhar} onChange={handleChange} className="border border-black rounded-md p-1 m-4" placeholder="Valid Aadhar Number"></input>
                 </div>
 
-                <div className="border border-black rounded-md my-2 shadow-md">
+                <div className="border border-black rounded-md bg-purple-50 my-2 shadow-md">
                     <h3 className="text-lg underline font-semibold font-serif ml-4 mt-2 shadow-sm">Account and Security</h3>
                     <PasswordInput name="password" value={adopterData.password} onChange={handleChange} placeholder="Enter your password"></PasswordInput>
                 </div>
 
-                <button className="self-center hover:underline text-base font-semibold font-serif mt-6 shadow-md border-2 border-black rounded-md p-2">Submit</button>
+                <button className="self-center text-base font-semibold font-serif mt-6 shadow-md border-2 border-black rounded-md p-2  bg-blue-50 hover:underline">Submit</button>
                 
             </div>
         </div>
