9d0b285f55fc567c75c809bfc294c83c015964c8
Shreyansh <shreyanshchaudhary99@gmail.com>
2025-05-28 16:50:20 +0530

    navbar changes final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,33 @@
-import React, { useState } from 'react';
-
+import { useState } from 'react';
+import NgoApprStatus from '../components/Home_Components/NgoApprStatus';
 import Hero from "../components/Home_Components/Hero";
 import Navbar from "../components/Home_Components/navbar";
 import Login from "../components/Home_Components/Login";
-import NgoApprStatus from '../components/Home_Components/NgoApprStatus';
 import Footer from '../components/Home_Components/Footer';
+
 function Home() {
- 
   const [showLogin, setShowLogin] = useState(false);
   const[showNgoApr,setNgoApr]=useState(false);
-  
+
   const handleShowLogin = () => {
     setShowLogin(!showLogin);
     setNgoApr(false);
-   
   };
+
   const handleShowNgoApr=()=>{
     setNgoApr(!showNgoApr);
     setShowLogin(false);
-  };
+  };
   
   return (
     <div className="relative w-full h-screen">
-      <Navbar onLoginClick={handleShowLogin} onNGOaprClick={handleShowNgoApr}/>
+      <Navbar onLoginClick={handleShowLogin} onNGOaprClick={handleShowNgoApr} />
       <Hero onGetStartedClick={handleShowLogin}  />
+
       {showNgoApr&&<NgoApprStatus />}
       {showLogin && <Login />}
-      <Footer/>
+
+      <Footer />
     </div>
   );
 }
